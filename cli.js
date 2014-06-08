4f5ddcef15d3c126213742b6f6f8720f1f2b687c
Jason Frame <jason@onehackoranother.com>
2014-06-08 18:45:59 +0100

    tabs -> spaces again

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
-var fs 			= require('fs');
-var Package 	= require('./lib/Package');
+var fs          = require('fs');
+var Package     = require('./lib/Package');
 
 var options = require('docopt').docopt(fs.readFileSync(__dirname + '/usage.txt', 'utf8'), {
     help        : true,
@@ -7,36 +7,36 @@ var options = require('docopt').docopt(fs.readFileSync(__dirname + '/usage.txt',
 });
 
 var commands = {
-	'create' 		: require('./lib/handlers/create_project'),
-	'create-module'	: require('./lib/handlers/create_module'),
-	'install' 		: require('./lib/handlers/install_module'),
-	'build' 		: require('./lib/handlers/build_project'),
-	'regen'			: require('./lib/handlers/regenerate')
+    'create'        : require('./lib/handlers/create_project'),
+    'create-module' : require('./lib/handlers/create_module'),
+    'install'       : require('./lib/handlers/install_module'),
+    'build'         : require('./lib/handlers/build_project'),
+    'regen'         : require('./lib/handlers/regenerate')
 };
 
 var pkg = null;
 try {
-	pkg = Package.tryLoadFromDirectory(null, '.');
+    pkg = Package.tryLoadFromDirectory(null, '.');
 } catch (e) {
-	console.error("error loading package!");
-	console.error(e);
+    console.error("error loading package!");
+    console.error(e);
 }
 
 var command = null;
 
 for (var k in commands) {
-	if (options[k]) {
-		command = commands[k](pkg, options);
-		break;
-	}
+    if (options[k]) {
+        command = commands[k](pkg, options);
+        break;
+    }
 }
 
 command
-	.then(function() {
-		console.log("DONE!");
-	})
-	.catch(function(err) {
-		console.error("ERROR");
-		console.error(err);
-		console.error(err.stack);
-	});
+    .then(function() {
+        console.log("DONE!");
+    })
+    .catch(function(err) {
+        console.error("ERROR");
+        console.error(err);
+        console.error(err.stack);
+    });
