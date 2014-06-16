e6cf9b78556405ecac6e216863a3a22d448b528f
Jason Frame <jason@onehackoranother.com>
2014-06-16 17:35:25 +0100

    mass reorganisation and some new handlers

--- cli.js
+++ cli.js
@@ -1,22 +1,11 @@
 var fs          = require('fs');
+var commands    = require('./lib/commands');
 var Package     = require('./lib/Package');
 
 var options = require('docopt').docopt(fs.readFileSync(__dirname + '/usage.txt', 'utf8'), {
     help        : true,
     version     : require('./package.json').version
 });
-
-var commands = {
-    'create'        : require('./lib/handlers/create_app'),
-    'create-module' : require('./lib/handlers/create_module'),
-    'install'       : require('./lib/handlers/install_package'),
-    'build'         : require('./lib/handlers/build_package'),
-    'make'          : require('./lib/handlers/build_make_target'),
-    'regen'         : require('./lib/handlers/regenerate'),
-    'invalidate'    : require('./lib/handlers/invalidate_package'),
-    'config'        : require('./lib/handlers/dump_config'),
-    'env'           : require('./lib/handlers/dump_environment')
-};
 
 var pkg = null;
 try {
